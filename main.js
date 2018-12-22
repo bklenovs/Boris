972080e26f1cc229f0c16907303b8eacfcf4e28e
Boris <boris.klenovsek@outlook.com>
2018-12-22 14:33:13 +0100

    ok

--- main.js
+++ main.js
@@ -1,25 +1,12 @@
-const {app, BrowserWindow} = require('electron')
-let mainWindow
+const { app, BrowserWindow, Menu } = require('electron')
 
-function createWindow () {
-  mainWindow = new BrowserWindow({width: 800, height: 600, frame:false})
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({ height: 600, width: 600 })
   mainWindow.loadFile('index.html')
-  mainWindow.on('closed', function () {
-    mainWindow = null
-  })
-}
+  }),
 
-app.on('ready', createWindow)
-
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-
-app.on('activate', function () {
-  if (mainWindow === null) {
-    createWindow()
-  }
+app.on('ready', () => {
+  const template = [{ label: 'Lep pozdrav' }]
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)	
 })
